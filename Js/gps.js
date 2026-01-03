b6b911656b31bb5e39f0a6c46cac730f9656f740
322103310205 <137491360+322103310205@users.noreply.github.com>
2026-01-03 15:21:14 +0530

    Add comments to gps.js for future reference
    Added comments explaining the purpose of the file.

--- Js/gps.js
+++ Js/gps.js
@@ -1,1 +1,9 @@
-export let userHeading = { x: 0, y: -1 };
+/* 
+  GPS is NOT used in marker-based navigation.
+  This file is kept only for future extension.
+*/
+
+let userHeading = {
+  x: 0,
+  y: -1
+};
